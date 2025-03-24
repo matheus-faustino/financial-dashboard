c56e65b3708a531eabe65b24d48cd43ba68c9e34
matheus-faustino <matheus.melo.faus@gmail.com>
2025-03-24 15:11:07 +0000

    Fix model in TagFactory signature

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
  */
 class TagFactory extends Factory
 {

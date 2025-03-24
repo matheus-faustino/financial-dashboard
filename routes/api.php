09a261c7d88bcefb1c2e8926a5c84f4ce0e19197
matheus-faustino <matheus.melo.faus@gmail.com>
2025-03-24 16:31:41 +0000

    Change API resource routes priority to avoid conflicts

--- routes/api.php
+++ routes/api.php
@@ -27,31 +27,31 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);
 
     // Transactions
-    Route::apiResource('transactions', TransactionController::class);
     Route::get('/transactions/by-category/{categoryId}', [TransactionController::class, 'byCategory']);
     Route::get('/transactions/by-tag/{tagId}', [TransactionController::class, 'byTag']);
     Route::get('/transactions/recurring', [TransactionController::class, 'recurring']);
     Route::post('/transactions/{transaction}/tags', [TransactionController::class, 'addTags']);
     Route::delete('/transactions/{transaction}/tags', [TransactionController::class, 'removeTags']);
+    Route::apiResource('transactions', TransactionController::class);
 
     // Categories
-    Route::apiResource('categories', CategoryController::class);
     Route::get('/categories/by-type/{type}', [CategoryController::class, 'byType']);
     Route::get('/categories/system', [CategoryController::class, 'system']);
     Route::get('/categories/custom', [CategoryController::class, 'custom']);
     Route::post('/categories/merge', [CategoryController::class, 'merge']);
+    Route::apiResource('categories', CategoryController::class);
 
     // Tags
-    Route::apiResource('tags', TagController::class);
     Route::get('/tags/by-frequency', [TagController::class, 'byFrequency']);
     Route::get('/tags/search', [TagController::class, 'search']);
+    Route::apiResource('tags', TagController::class);
 
     // Users - only accessible by admins and managers
     Route::middleware('can:manage-users')->group(function () {
-        Route::apiResource('users', UserController::class);
         Route::get('/users/by-role/{role}', [UserController::class, 'byRole']);
         Route::get('/users/by-manager/{managerId}', [UserController::class, 'byManager']);
         Route::patch('/users/{user}/status', [UserController::class, 'updateStatus']);
         Route::post('/users/{user}/password', [UserController::class, 'changePassword'])->middleware('can:reset-password,user');
+        Route::apiResource('users', UserController::class);
     });
 });
